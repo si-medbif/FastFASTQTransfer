4095ed4795c2809eaf4282898caa963fa83f4678
Arnon Puitrakul <peter_arnon1@icloud.com>
2020-09-18 22:25:34 +0700

    Add Experiment on Huffman and Arithmetic Encoding

--- Experiments/Pilot_Compression/pilot_compression_test.py
+++ Experiments/Pilot_Compression/pilot_compression_test.py
@@ -2,110 +2,98 @@ import os
 import sys
 import time
 import glob
+import tracemalloc
 
-## Measuring Compression and Decompression Time Spending by using Generic Compression (gzip)
-def is_gzipped (file_name) :
-    if file_name.split('.')[-1] == 'gz' :
-        return True
-    else:
-        return False
+## Measuring Compression and Decompression Time Spending by using Generic Compression (gzip, Arithmetic Encoding and Huffman Encoding)
+def memory_measurement_closure (command) :
+    # Return [Current Memory, Peak Memory]
+    tracemalloc.start()
+    os.system(command)
+    return tracemalloc.get_traced_memory()
 
-def unzip_merge_paired_end (base_file_name) :
-    os.system("gunzip " + base_file_name + "_1.fastq.gz " + base_file_name + "_2.fastq.gz")
-    
-    # Merge File
-    os.system("cat " + base_file_name + "_1.fastq " + base_file_name + "_2.fastq > " + base_file_name + ".fastq")
+def run_experiment_pipeline (compression_command, decompression_command, no_of_run, output_file_path, compressed_file_name, file_name='', remove_after_finished=False) :
+    output_file = open(output_file_path, 'w')
+    output_file.write('Experiment No.,Compressed Size (Byte),Compression Time (Sec),Decompression Time (Sec),Compression Memory (MB), Decompression Memory (MB)\n')
 
-def calculate_transfer_time (file_size, speed=1*10**8) :
-    # Input : File Size (Byte), Transfer Speed (bps) ; default 100 Mbps
-    return (file_size * 8) / speed
-
-def parse_arguments (args) :
-    # Add Default Configurations
-    if len(args) < 2 :
-        exit("Input File is missing")
-
-    result = {
-        "input_file" : args[1],
-        "output_file": 'output.o',
-        "no_of_run" : 10
-    }
-
-    for arg in args :
-        if arg[:2] == '-n' :
-            result['no_of_run'] = arg[3:]
-        elif arg[:2] == '-o' :
-            result['output_file'] = arg[3:]
-
-    return result
-
-def get_file_name (path_to_file) :
-    splited_path = path_to_file.split('/')
-
-    if len(splited_path) == 1 :
-        return path_to_file
-    else :
-        return splited_path[-1]
-
-def prepare_file_list () :
-    original_file_list = glob.glob('*.fastq.gz')
-    file_list = []
-    for file_name in original_file_list :
-        file_list.append(file_name.split('_')[0])
-    file_list = list(set(file_list))
-    return file_list
-
-def run_experiment (file_name, no_of_run, output_file) :
-    # Compression and Decompression Experiment
-    avg_compress_size, avg_compression_time, avg_decompression_time, avg_transmit_time = 0,0,0,0
+    whole_compressed_size, whole_compression_time, whole_decompression_time, whole_compression_memory, whole_decompression_memory = 0,0,0,0,0
 
     for i in range(no_of_run) :
         # Compression Process
-        start_time = time.time()
-        os.system('gzip ' + file_name)
-        compression_time = time.time() - start_time
-        avg_compression_time += compression_time
+        compression_start_time = time.time()
+        compression_peak_memory = memory_measurement_closure(compression_command + ' ' + file_name)[1]
+        compression_time = time.time() - compression_start_time
 
-        # Measured Compressed Size
-        compressed_file_size = os.stat(file_name + '.gz').st_size
-        avg_compress_size += compressed_file_size
+        # Measure Compressed File Size
+        compressed_file_size = os.stat(compressed_file_name).st_size
+        whole_compressed_size += compressed_file_size
 
         # Decompression Process
-        start_time = time.time()
-        os.system('gzip -d ' + file_name + '.gz')
-        decompression_time = time.time() - start_time
-        avg_decompression_time += decompression_time
+        decompression_start_time = time.time()
+        decompression_peak_memory = memory_measurement_closure(decompression_command + ' ' + file_name)[1]
+        decompression_time = time.time() - decompression_start_time
 
-        # Get Tranfer Time
-        transmit_time = calculate_transfer_time(compressed_file_size)
-        avg_transmit_time += transmit_time
+        whole_compression_time += compression_time
+        whole_decompression_time += decompression_time
+        whole_compression_memory += compression_peak_memory / 10**6
+        whole_decompression_memory += decompression_peak_memory / 10**6
 
-        output_file.write(str([i, file_name, os.stat(file_name).st_size, compressed_file_size, transmit_time, compression_time, decompression_time])[1:-1].replace(', ', ',') + '\n')
+        # Write the result back to result file
+        output_file.write(str(i) + ',' + str(compressed_file_size) + ',' + str(compression_time) + ',' + str(decompression_time) + '\n')
+    
+    output_file.write('-1,' + str(whole_compressed_size/no_of_run) + ',' + str(whole_compression_time/no_of_run) + ',' + str(whole_decompression_time/no_of_run) + '\n')
+    output_file.close()
 
-        return avg_compress_size/no_of_run, avg_compression_time/no_of_run, avg_decompression_time/no_of_run, avg_transmit_time/no_of_run
+    if remove_after_finished :
+        os.system('rm ' + compressed_file_name)
+
+def run_batch_gzip_experiment (file_list, no_of_run, output_file_path) :
+    for file_name in file_list :
+        run_experiment_pipeline(
+            compression_command='gzip -fk ',
+            decompression_command='gzip -fkd ',
+            no_of_run=no_of_run,
+            output_file_path=output_file_path + '/gzip_' + file_name.split('.')[0],
+            compressed_file_name=file_name + '.gz',
+            file_name=file_name,
+            remove_after_finished=True
+            )
+
+def run_batch_arithmetic_experiment (file_list, no_of_run, output_file_path) :
+    for file_name in file_list :
+        run_experiment_pipeline(
+            compression_command='python3 Experiments/Pilot_Compression/reference-arithmetic-coding/python/arithmetic-compress.py ' + file_name + ' ' + file_name + '.bin', 
+            decompression_command='python3 Experiments/Pilot_Compression/reference-arithmetic-coding/python/arithmetic-decompress.py ' + file_name + '.bin ' + file_name, 
+            no_of_run = no_of_run, 
+            output_file_path = output_file_path + '/arithmetic_' + file_name.split('.')[0], 
+            compressed_file_name = file_name + '.bin', 
+            file_name = file_name, 
+            remove_after_finished=True )
+
+def run_batch_huffman_experiment (file_list, no_of_run, output_file_path) :
+    for file_name in file_list :
+        run_experiment_pipeline(
+            compression_command='python3 Experiments/Pilot_Compression/Huffman-Coding/Huffman.py -c ' + file_name, 
+            decompression_command='python3 Experiments/Pilot_Compression/Huffman-Coding/Huffman.py -d ' + file_name + '.huffman', 
+            no_of_run = no_of_run, 
+            output_file_path = output_file_path + '/arithmetic_' + file_name.split('.')[0], 
+            compressed_file_name = file_name + '.huffman', 
+            file_name = file_name, 
+            remove_after_finished=True )
 
 def main(args) :
-    # Get Base File Name
-    file_list = prepare_file_list()
+    source_folder = args[1]
+    result_folder = args[2]
+    no_of_run = int(args[3])
 
-    for base_file_name in file_list :
-        unzip_merge_paired_end(base_file_name)
-        input_file_name = base_file_name + ".fastq"
+    file_list = glob.glob(source_folder)
 
+    print('There are ', len(file_list), ' to run.')
 
-        print("Run Experiment", args['no_of_run'], ' run(s) on ', input_file_name)
-
-        output_file = open(args['output_file'], 'a')
-
-        # Append Header
-        output_file.write("Experiment Number,File Name,Original Size,Compressed Size,Transmitted Time,Compression Time,Decompression Time\n")
-
-        avg_compress_size, avg_compression_time, avg_decompression_time, avg_transmit_time = run_experiment(input_file_name, args['no_of_run'], output_file)
-
-        # Write Final Line with Average
-        output_file.write(str([-1, input_file_name, os.stat(input_file_name).st_size, avg_compress_size, avg_transmit_time, avg_compression_time, avg_decompression_time])[1:-1].replace(', ', ',') + "\n")
-        output_file.close()
+    # Run Each Experiment Set
+    run_batch_gzip_experiment(file_list, no_of_run, result_folder)
+    run_batch_arithmetic_experiment(file_list, no_of_run, result_folder)
+    run_batch_huffman_experiment(file_list, no_of_run, result_folder)
 
 if __name__ == "__main__":
-    arguments = parse_arguments(sys.argv)
-    main(arguments)
+    # python3 pilot_compression_test.py <Source_Folder> <Result_Folder> <Number of Run Per Experiment and File>
+    main(sys.argv)
