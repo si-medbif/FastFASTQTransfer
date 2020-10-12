8ded1ca21613f238cbf766c40a1b1db5f0e5ad75
Arnon Puitrakul <peter_arnon1@icloud.com>
2020-10-12 21:15:21 +0700

    Experiment new generator with Dask

--- sender/model_service.py
+++ sender/model_service.py
@@ -1,5 +1,7 @@
+import math
 import pandas as pd
 import numpy as np
+import dask.dataframe as dd
 
 from tensorflow.keras.models import Sequential 
 from tensorflow.keras.utils import to_categorical
@@ -10,40 +12,34 @@ def preprocess_score_to_prob (input_y) :
     # Quality Score Has 0-42 (43 Categorical Class Possible)
     return to_categorical(input_y, 43)
 
-def load_data_as_batch (data_path, index, batch_size, model_position=None) :
-    dataset = pd.read_csv(data_path, skiprows=index*batch_size, nrows=batch_size)
-
-    no_of_feature = int(len(dataset.columns) / 2)
-
-    # Seperate X and Y by Half
-    x = dataset.iloc[:, :no_of_feature]
-
-    if model_position != None :
-        # The Score Position is Selected -> Grab only prefered position and transform
-        y = preprocess_score_to_prob(dataset.iloc[:, no_of_feature + model_position-1])
-    else :
-        y = dataset.iloc[:, no_of_feature:]
-
-    return (np.array(x), np.array(y))
-
 def batch_generator (data_path, batch_size, steps, model_position=None) :
     index = 1
+    dataset = dd.read_csv(data_path, header=None)
+    no_of_feature = math.floor(len(dataset.columns) / 2)
+
     while True :
-        yield load_data_as_batch(data_path, index-1, batch_size, model_position=model_position)
+        # Seperate X and Y
+        x = dataset.loc[index].iloc[:,:no_of_feature].compute()
+
+        if model_position is not None :
+            y = preprocess_score_to_prob(dataset.loc[index].iloc[:, no_of_feature + model_position - 1].compute())
+        else :
+            y = dataset.loc[index].iloc[:, no_of_feature:].compute()
+        
+        yield np.array(x), np.array(y)
+        
         if index < steps :
             index += 1
         else :
             index = 1
 
-def train_sequencial_model (layers, feature_file, epoch=100, optimiser="adam", loss="categorical_crossentropy", step_per_epoch=20000, model_position=None) :
+def train_sequencial_model (layers, feature_file, epoch=100, optimiser="adam", loss="categorical_crossentropy", step_per_epoch=20000, batch_size=10000, model_position=None) :
     data_batch_generator = batch_generator(feature_file, 256, step_per_epoch, model_position=model_position)
 
-    model = Sequential()
-    for layer in layers :
-        model.add(layer)
+    model = Sequential(layers)
 
     model.compile(optimizer=optimiser, loss=loss, metrics=['accuracy', 'mse'])    
 
-    training_hist = model.fit_generator(data_batch_generator, epochs=epoch, steps_per_epoch=step_per_epoch, validation_data=data_batch_generator, validation_steps=step_per_epoch)
+    training_hist = model.fit(data_batch_generator, epochs=epoch, steps_per_epoch=step_per_epoch, validation_data=data_batch_generator, validation_steps=step_per_epoch)
 
     return model, training_hist
