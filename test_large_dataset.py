61e742b7fe555888e66ef77f80c6526d855d1160
Arnon Puitrakul <peter_arnon1@hotmail.com>
2020-10-25 17:19:21 +0700

    Add Model Experiment 4-6

--- test_large_dataset.py
+++ test_large_dataset.py
@@ -43,36 +43,122 @@ def evaluate_model (training_history, experiment_name) :
     plt.cla()
     plt.close()
 
+def model_training_experiment_set (feature_file_path, model_configuration, layer_configuration, experiment_name) :
+
+    # Train and Evaluate Model
+    model, training_hist = train_sequencial_model(layer_configuration, feature_file_path, epoch=model_configuration['no_of_epoch'], step_per_epoch=model_configuration['steps_per_epoch'], model_position=1)    
+    evaluate_model(training_hist, experiment_name)
+    
+    # Save trained model to file
+    model.save('Results/model_experiment/model/' + experiment_name + '.h5')
+
 def main (args) :    
     # Input Parameter
     read_file_path, qscore_file_path, feature_file_path = args[1], args[2], args[3]
     
     # Producing Feature File From Read and Quality Score File
-    # parallel_extract_feature (read_file_path, qscore_file_path, feature_file_path) 
-    
-    # Training Configurations
-    input_dim = 90
-    output_dim = 43
-    no_of_epoch = 10
-    steps_per_epoch = 200000
+    # parallel_extract_feature (read_file_path, qscore_file_path, feature_file_path)
 
-    # Experiment Detail
-    experiment_name = "3_Layers_Softmax"
+    # Training Configurations
+    model_configuration = {
+        'input_dim' : 90,
+        'output_dim' : 43,
+        'no_of_epoch' : 10,
+        'steps_per_epoch': 200000
+    }
+
+    # # EXPERIMENT 1 STARTS HERE
+
+    # # Layer Container
+    # model_layers = []
+
+    # # Layer Specification
+    # model_layers.append(Dense(model_configuration['input_dim'], activation='softmax'))
+    # model_layers.append(Dense(50, activation='softmax'))
+    # model_layers.append(Dense(model_configuration['output_dim'], activation='softmax'))
+
+    # model_training_experiment_set(feature_file_path, model_configuration, layer_configuration=model_layers, experiment_name='3_Layers_Softmax_50L2')
+
+    # # END OF EXPERIMENT 1
+
+    # # EXPERIMENT 2 STARTS HERE
+
+    # # Layer Container
+    # model_layers = []
+
+    # # Layer Specification
+    # model_layers.append(Dense(model_configuration['input_dim'], activation='softmax'))
+    # model_layers.append(Dense(500, activation='softmax'))
+    # model_layers.append(Dense(model_configuration['output_dim'], activation='softmax'))
+
+    # model_training_experiment_set(feature_file_path, model_configuration, layer_configuration=model_layers, experiment_name='3_Layers_Softmax_500L2')
+
+    # # END OF EXPERIMENT 2
+
+    # # EXPERIMENT 3 STARTS HERE
+
+    # # Layer Container
+    # model_layers = []
+
+    # # Layer Specification
+    # model_layers.append(Dense(model_configuration['input_dim'], activation='softmax'))
+    # model_layers.append(Dense(200, activation='softmax'))
+    # model_layers.append(Dense(100, activation='softmax'))
+    # model_layers.append(Dense(model_configuration['output_dim'], activation='softmax'))
+
+    # model_training_experiment_set(feature_file_path, model_configuration, layer_configuration=model_layers, experiment_name='4_Layers_Softmax_200L2_100L3')
+
+    # # END OF EXPERIMENT 3
+
+    # EXPERIMENT 4 STARTS HERE
 
     # Layer Container
     model_layers = []
 
     # Layer Specification
-    model_layers.append(Dense(input_dim, activation='softmax'))
+    model_layers.append(Dense(model_configuration['input_dim'], activation='softmax'))
+    model_layers.append(Dense(300, activation='softmax'))
     model_layers.append(Dense(200, activation='softmax'))
-    model_layers.append(Dense(output_dim, activation='softmax'))
+    model_layers.append(Dense(100, activation='softmax'))
+    model_layers.append(Dense(model_configuration['output_dim'], activation='softmax'))
 
-    # Train and Evaluate Model
-    model, training_hist = train_sequencial_model(model_layers, feature_file_path, epoch=no_of_epoch, step_per_epoch=steps_per_epoch, model_position=1)    
-    evaluate_model(training_hist, experiment_name)
-    
-    # Save trained model to file
-    model.save('Results/model_experiment/model/' + experiment_name + '.h5')
+    model_training_experiment_set(feature_file_path, model_configuration, layer_configuration=model_layers, experiment_name='Model_4')
+
+    # END OF EXPERIMENT 4
+
+    # EXPERIMENT 5 STARTS HERE
+
+    # Layer Container
+    model_layers = []
+
+    # Layer Specification
+    model_layers.append(Dense(model_configuration['input_dim'], activation='softmax'))
+    model_layers.append(Dense(500, activation='softmax'))
+    model_layers.append(Dense(500, activation='softmax'))
+    model_layers.append(Dense(500, activation='softmax'))
+    model_layers.append(Dense(model_configuration['output_dim'], activation='softmax'))
+
+    model_training_experiment_set(feature_file_path, model_configuration, layer_configuration=model_layers, experiment_name='Model_5')
+
+    # END OF EXPERIMENT 5
+
+    # EXPERIMENT 6 STARTS HERE
+
+    # Layer Container
+    model_layers = []
+
+    # Layer Specification
+    model_layers.append(Dense(model_configuration['input_dim'], activation='softmax'))
+    model_layers.append(Dense(500, activation='softmax'))
+    model_layers.append(Dense(500, activation='softmax'))
+    model_layers.append(Dense(500, activation='softmax'))
+    model_layers.append(Dense(500, activation='softmax'))
+    model_layers.append(Dense(500, activation='softmax'))
+    model_layers.append(Dense(model_configuration['output_dim'], activation='softmax'))
+
+    model_training_experiment_set(feature_file_path, model_configuration, layer_configuration=model_layers, experiment_name='4_Layers_Softmax_200L2_100L3')
+
+    # END OF EXPERIMENT 6
 
 if __name__ == "__main__":
     # python test_large_dataset.py <Read File> <Quality Score File> <Feature File Path>
