f716042b016f7ccf527f1fb882b3de42d8147e0d
Arnon Puitrakul <peter_arnon1@icloud.com>
2020-11-24 13:11:59 +0700

    Add feature extraction function

--- Experiments/Model/data_preprocessing.py
+++ Experiments/Model/data_preprocessing.py
@@ -1,5 +1,8 @@
 from joblib import Parallel, delayed
+from tensorflow.keras.utils import to_categorical
 
+import pandas as pd
+import math
 import glob
 import os
 
@@ -21,3 +24,15 @@ def extract_read_and_quality_from_fastq (fastq_path, read_path, quality_path) :
             delayed(os.system)(command_job)
             for command_job in jobs
     )
+
+# Feature Extraction
+
+def extract_x_y_from_pandas (full_data_frame, model_position=None) :
+# Extract X and Y from Whole Dataset Dataframe
+    n_columns = len(full_data_frame.columns)
+    n_feature = math.floor(n_columns / 2)
+
+    if model_position is None :
+        return full_data_frame.iloc[:, :n_feature], full_data_frame.iloc[:, n_feature:]
+    else :
+        return full_data_frame.iloc[:, :n_feature], full_data_frame.iloc[:, n_feature - 1 + model_position]
