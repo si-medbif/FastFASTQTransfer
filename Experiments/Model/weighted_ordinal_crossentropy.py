1fef6061f00e8eb1b3c8237833850160b78373ad
DumMai <dumrong_m@yahoo.com>
2020-12-22 14:01:34 +0700

    Create weighted_ordinal_crossentropy.py
    Modified from weighted_categorical_crossentropy.py

---
+++ Experiments/Model/weighted_ordinal_crossentropy.py
@@ -0,0 +1,30 @@
+def weighted_ordinal_crossentropy(weights):
+    """
+    A weighted version of keras.objectives.categorical_crossentropy
+    
+    Variables:
+        weights: numpy array of shape (C,) where C is the number of classes
+    
+    Usage:
+        weights = np.array([0.5,2,10]) # Class one at 0.5, class 2 twice the normal weights, class 3 10x.
+        loss = weighted_categorical_crossentropy(weights)
+        model.compile(loss=loss,optimizer='adam')
+    """
+    
+    weights = K.variable(weights)
+    
+    def ordinal_loss_exp(y_true, y_pred):
+        # Add exponential ordinal weights
+        ord_weights = K.cast(K.abs(K.argmax(y_true, axis=1) - K.argmax(y_pred, axis=1))/(K.int_shape(y_pred)[1] - 1), dtype='float32')
+        # scale predictions so that the class probas of each sample sum to 1
+        y_pred /= K.sum(y_pred, axis=-1, keepdims=True)
+        # clip to prevent NaN's and Inf's
+        y_pred = K.clip(y_pred, K.epsilon(), 1 - K.epsilon())
+        # calc
+        loss = y_true * K.log(y_pred) * weights
+        loss = -K.sum(loss, -1)
+        loss = (1.0 + K.exp(ord_weights)) * loss
+        # If you want to use linear weight, comment the line above and uncomment the line below.
+        # loss = (1.0 + ord_weights) * loss
+        return loss
+    return ordinal_loss_exp
