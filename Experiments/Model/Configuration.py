9ef2d103f41f8327df0604c88dc8bdb0c1a2e745
Arnon Puitrakul <peter_arnon1@icloud.com>
2021-01-06 20:11:32 +0700

    Add new config item

--- Experiments/Model/Configuration.py
+++ Experiments/Model/Configuration.py
@@ -4,11 +4,12 @@ import math
 # Store the model configuration for each experiment
 
 class Configuration :
-    def __init__ (self, experiment_name, latent_dim=256, num_encoder_tokens = 5, num_decoder_tokens = 41, num_decoder_embed = 2, seq_num= 10000, seq_len = 90, base_learning_rate=0.01, batch_size=10, loss='categorical_crossentropy') :
+    def __init__ (self, experiment_name, latent_dim=256, num_encoder_tokens = 5, num_decoder_tokens = 41, num_encoder_embed = 2, num_decoder_embed = 32, seq_num= 10000, seq_len = 90, base_learning_rate=0.01, batch_size=10, loss='categorical_crossentropy') :
         self.experiment_name = experiment_name
         self.latent_dim = latent_dim
         self.num_encoder_tokens = num_encoder_tokens
         self.num_decoder_tokens = num_decoder_tokens
+        self.num_encoder_embed = num_encoder_embed
         self.num_decoder_embed = num_decoder_embed
         self.seq_num = seq_num
         self.seq_len = seq_len
